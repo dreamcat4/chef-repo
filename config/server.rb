a02ee07ec3906c235a5045bb54a6232ea30cc8b8
dreamcat4 <dreamcat4@gmail.com>
2009-12-01 14:54:11 +0000

    Remove config folder (now use cookbooks erb template)

--- config/server.rb
+++
@@ -1,8 +0,0 @@
-log_location STDOUT
-log_level :debug
-chef_server_url "http://chef.localdomain:4000"
-openid_url "http://chef.localdomain:4001"
-cookbook_path %w{/srv/chef/site-cookbooks /srv/chef/cookbooks}
-role_path "/srv/chef/roles"
-authorized_openid_identifiers [ "dreamcat4.myopenid.com" ]
-validation_token "162a81fc9c30ade3e021ea084d72df29"
